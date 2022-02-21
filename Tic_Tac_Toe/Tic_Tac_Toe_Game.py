3ff3c6c27c3f7be892014f4b0d78d7b1871f598b
jkowalski995 <kuba.ko.1995@gmail.com>
2022-02-21 22:36:51 +0100

    Tic_Tac_Toe_v1

--- Tic_Tac_Toe/Tic_Tac_Toe_Game.py
+++ Tic_Tac_Toe/Tic_Tac_Toe_Game.py
@@ -1,3 +1,11 @@
+import functions
+
 # To Do:
-# in functions put all functions from exercises
+# player two can't win
 # check if everything works properly
+while True:
+    check = input("Do You want to play? (Y)es or (N)o")
+    if check.lower() == "y":
+        functions.game_ttt()
+    else:
+        break
